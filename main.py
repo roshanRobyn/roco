c03c8d423c4c1f4051d2088981a3e04df7526f15
Roshan Robin <roshanrobinc@gmail.com>
2025-12-10 14:20:26 +0530

    Replaced dummy output with pasting to notebook

--- main.py
+++ main.py
@@ -4,11 +4,28 @@ import pyperclip
 import os
 import subprocess
 import keyboard
-
+import psutil
 log_file="RoCopy.txt"
 
 def rocopy():
     keyboard.send('ctrl+c')
+def append_to_txt(text):
+    try:
+        with open(log_file,"a",encoding="utf-8") as f:
+            f.write(text+"\n")
+        if not is_notepad_open():
+            subprocess.Popen(["notepad.exe",log_file])
+    except Exception as e:
+        print(e)
+    
+def is_notepad_open():
+    for proc in psutil.process_iter(['name']):
+        try:
+            if proc.info['name']=='notepad.exe':
+                return True
+        except (psutil.NoSuchProcess,psutil.AccessDenied,psutil.ZombieProcess):
+            pass
+    return False
 
 def monitor_clip():
     last_text=""
@@ -20,12 +37,12 @@ def monitor_clip():
         try:
             current=pyperclip.paste()
             if current!=last_text and current.strip() !="":
-                print(f"text was copied  {current}")
+                append_to_txt(current)
                 last_text=current
         except Exception as e:
             print(e)
         time.sleep(0.5)
-
+keyboard.add_hotkey('f7',rocopy,suppress=True)
 clipboard_thread=threading.Thread(target=monitor_clip,daemon=True)
 clipboard_thread.start()            
 keyboard.wait('f8',suppress=True)
